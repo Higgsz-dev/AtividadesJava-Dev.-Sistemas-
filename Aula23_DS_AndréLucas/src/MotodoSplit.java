654bfd11c706f2ae19fac4e173fd3ba7ca4de45c
André Lucas <71662736+LAFSHC@users.noreply.github.com>
2021-04-01 15:55:54 -0300

    Atividade Aula 23
    Atividade usando metodo Split

---
+++ Aula23_DS_AndréLucas/src/MotodoSplit.java
@@ -0,0 +1,25 @@
+
+public class MotodoSplit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String texto = "| 1/|  22/|   333/|    4444/|     55555/|      666666/>>_______7777777>";
+		String[] array = texto.split("/");
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+		System.out.println(array[2]);
+		System.out.println(array[3]);
+		System.out.println(array[4]);
+		System.out.println(array[5]);
+		System.out.println(array[5]);
+		System.out.println(array[6]);
+		System.out.println(array[5]);
+		System.out.println(array[5]);
+		System.out.println(array[4]);
+		System.out.println(array[3]);
+		System.out.println(array[2]);
+		System.out.println(array[1]);
+		System.out.println(array[0]);
+		
+	}
+}
